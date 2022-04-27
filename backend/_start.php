70b953c118873b2349140d483694f666f7887939
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-04-27 19:34:39 +0200

    initial work on post add

--- backend/_start.php
+++ backend/_start.php
@@ -12,6 +12,10 @@ function get_letezik($kulcs)
     return isset($_GET[$kulcs]) && strlen($_GET[$kulcs])>0 ? $_GET[$kulcs] : false;
 }
 
+function post_isset($kulcs){
+    return isset($_POST[$kulcs]) ? $_POST[$kulcs] : false;
+}
+
 function post_letezik($kulcs)
 {
     return isset($_POST[$kulcs]) && strlen($_POST[$kulcs])>0 ? $_POST[$kulcs] : false;
--- backend/templates/header.php
+++ backend/templates/header.php
@@ -16,5 +16,31 @@
         h1{
             text-transform: none;
         }
+        details{
+            background-color: #517d81;
+            color: white;
+            padding: 1em;
+            border: 10px solid #517d81;
+            transition: ease-in 0.2s;
+        }
+
+        details[open]{
+            background-color: initial;
+            color: black;
+            padding: 1em;
+            border: 10px solid #517d81;
+            transition: ease-in 0.2s;
+        }
+
+        summary{
+            width: 100%;
+            cursor: pointer;
+            margin-bottom: 0;
+        }
+
+        summary h3{
+            display: inline;
+        }
+
     </style>
 </head>
