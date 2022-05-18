03b54431c4565a6764e257ad6d3d71e83f047fd0
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-05-18 17:43:49 +0200

    frontend MVP

---
+++ backend/_get_next.php
@@ -0,0 +1,15 @@
+<?php
+require_once("_start.php");
+header("Access-Control-Allow-Origin: *");
+$ids = array_keys($slides->findAll(["active"=>true]));
+
+if(count($_GET) === 0 || !get_letezik("id") || !in_array($_GET["id"], $ids)) {
+    echo(json_encode($slides->findById($ids[0])));
+    die;
+}else{
+    $i = array_search($_GET["id"], $ids) +1;
+    if ($i === count($ids)){
+        $i = 0;
+    }
+    echo(json_encode($slides->findById($ids[$i])));
+}
