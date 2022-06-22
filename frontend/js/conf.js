d268dbb2199475a1c580deb6965bf7f3479bc0fb
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-06-22 21:34:04 +0200

    client: send client name for logging

--- frontend/js/conf.js
+++ frontend/js/conf.js
@@ -1,6 +1,7 @@
 const config = {
     backendUrl: 'http://localhost:3000',
-    timeout: 60
+    timeout: 60,
+    clientName: "bucsi-pc:local"
 }
 
 export default config
--- frontend/js/load.js
+++ frontend/js/load.js
@@ -3,7 +3,7 @@ import config from "./conf.js"
 export default async function load(old_id) {
     try {
         console.log("Trying to get next from " + old_id)
-        const res = await fetch(`${config.backendUrl}/_get_next.php?id=${old_id}`)
+        const res = await fetch(`${config.backendUrl}/_get_next.php?id=${old_id}&client=${config.clientName}`)
         const slide = await res.json()
         if (slide.file === "no active file") throw new Error("no active file")
         slide.file = `${config.backendUrl}/${slide.file}`
