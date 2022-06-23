88b7267f3eb61b635487aee8c35c6e99d089d6d8
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2022-06-23 14:21:15 +0200

    folder organization

--- src/client/js/conf.js
+++ src/client/js/conf.js
@@ -1,5 +1,5 @@
 const config = {
-    backendUrl: 'http://localhost:3000',
+    backendUrl: "http://webprogramozas.inf.elte.hu/hallgatok/dzhbcx/egyeb/ikijelzo/szerver",
     timeout: 60,
     clientName: localStorage.getItem("client-name") ?? "unnamed",
     delay: 0.6,
